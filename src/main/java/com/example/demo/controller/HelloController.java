985a5ccd79c9ccd491a193d2d477f3483caa8500
zui0202 <zui0202@naver.com>
2022-12-18 08:37:41 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public @ResponseBody String hello() {
+        return "Hello world";
+    }
+}
